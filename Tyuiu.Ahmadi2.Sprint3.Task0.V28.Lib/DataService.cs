4c50dab8317f77a8ac2876e6b6edf964b48c4132
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-06 07:51:35 -0800

    Task0

--- Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DataService.cs
+++ Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DataService.cs
@@ -2,17 +2,15 @@
 using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib
 {
-    public class DataService:ISprint3Task0V28
+    public class DataService :ISprint3Task0V28
     {
         public double GetMultiplySeries(double x)
         {
-            double product = 1;
+            double p = 1;
             for (int i = 1; i <= 17; i++)
             {
-                double term = Math.Pow(x, 3) * i + 2;
-                product *= term;
-            }
-            return Math.Round(product, 3);
+                p *= (Math.Pow(x, 3) + i) + 2;            }
+            return Math.Round(p, 3);
         }
 
         public double GetMultiplySeries(double value, int startValue, int stopValue)
---
+++ Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DtataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib;
+
+namespace Tyuiu.Ahmadi2.Sprint3.Task0.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.25;
+            double wait = 411589.537;
+
+            double res = ds.GetMultiplySeries(x);
+
+            Assert.AreEqual(wait, res, 0.001);
+        }
+    }
+}
