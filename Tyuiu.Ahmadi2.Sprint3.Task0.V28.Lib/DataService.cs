58edba7621ed1d36424e963e5b9fcff674f7451d
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-06 08:04:48 -0800

    task0

--- Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DataService.cs
+++ Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DataService.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using tyuiu.cources.programming.interfaces.Sprint3;
+
 namespace Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib
 {
-    public class DataService :ISprint3Task0V28
+    public class DataService : ISprint3Task0V28
     {
         public double GetMultiplySeries(double x)
         {
             double p = 1;
             for (int i = 1; i <= 17; i++)
             {
-                p *= (Math.Pow(x, 3) + i) + 2;            }
+                p *= (Math.Pow(x, 3) + i) + 2;
+            }
             return Math.Round(p, 3);
         }
 
-        public double GetMultiplySeries(double value, int startValue, int stopValue) 
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
         {
             throw new NotImplementedException();
         }
