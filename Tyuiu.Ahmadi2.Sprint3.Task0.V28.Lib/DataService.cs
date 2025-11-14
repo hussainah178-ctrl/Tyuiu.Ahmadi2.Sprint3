b78ea0242951a34164d41210e40145896fb60110
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-14 08:28:21 -0800

    Task0

--- Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DataService.cs
+++ Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DataService.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib
             double p = 1;
             for (int i = 1; i <= 17; i++)
             {
-                p *= (Math.Pow(x, 3) + i) * 2;
+                p *= (Math.Pow(x, 3) * i) + 2;
             }
             return Math.Round(p, 3);
         }
