8b0d623279a10321a1838ca76b7ed054d68bb889
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-14 09:12:52 -0800

    Task0

--- Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DataService.cs
+++ Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DataService.cs
@@ -9,16 +9,13 @@ namespace Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib
         {
             double p = 1;
             double x3 = Math.Pow(x, 3);
-            Console.WriteLine($"x³ = {x3}");
 
             for (int i = 1; i <= 17; i++)
             {
-                double term = x3 + i;
+                double term = x3 * i;
                 p *= term;
-                Console.WriteLine($"i={i}, term={term}, p={p}");
             }
             p += 2;
-            Console.WriteLine($"Final result: {p}");
             return Math.Round(p, 3);
         }
 
