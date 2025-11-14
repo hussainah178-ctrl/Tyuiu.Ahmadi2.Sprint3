519ff6d9ff45ab25037903f4960debaf95393d98
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-14 08:48:49 -0800

    Task0

--- Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DataService.cs
+++ Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib/DataService.cs
@@ -8,13 +8,20 @@ namespace Tyuiu.Ahmadi2.Sprint3.Task0.V28.Lib
         public double GetMultiplySeries(double x)
         {
             double p = 1;
+            double x3 = Math.Pow(x, 3);
+            Console.WriteLine($"x³ = {x3}");
+
             for (int i = 1; i <= 17; i++)
             {
-                p *= (Math.Pow(x, 3) + i); 
+                double term = x3 + i;
+                p *= term;
+                Console.WriteLine($"i={i}, term={term}, p={p}");
             }
-            p += 2; 
+            p += 2;
+            Console.WriteLine($"Final result: {p}");
             return Math.Round(p, 3);
         }
+
 
         public double GetMultiplySeries(double value, int startValue, int stopValue)
         {
