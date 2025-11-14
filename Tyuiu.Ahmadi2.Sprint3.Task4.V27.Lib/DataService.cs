7b93af4e5e08055b93274ad5374ae2a7d7152e1a
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-14 06:46:24 -0800

    Task4

--- Tyuiu.Ahmadi2.Sprint3.Task4.V27.Lib/DataService.cs
+++ Tyuiu.Ahmadi2.Sprint3.Task4.V27.Lib/DataService.cs
@@ -1,28 +1,26 @@
 ﻿using System;
-using tyuiu.cources.programming.interfaces.Sprint3;
 
 namespace Tyuiu.Ahmadi2.Sprint3.Task4.V27.Lib
 {
-    public class DataService : ISprint3Task4V27
+    public class DataService
     {
         public double Calculate(int startValue, int stopValue)
         {
             double sum = 0;
-            int x = startValue;
 
-            while (x <= stopValue)
+            for (int x = startValue; x <= stopValue; x++)
             {
-                if (x != 0) 
-                {
-                    double denominator = Math.Cos(x) - Math.Sin(x);
+                if (x == 0) continue;
 
-                    if (Math.Abs(denominator) > 0.0001)
-                    {
-                        double y = x / denominator;
-                        sum += y;
-                    }
+                double cosX = Math.Cos(x);
+                double sinX = Math.Sin(x);
+                double denominator = cosX - sinX;
+
+                if (Math.Abs(denominator) > 0.0001)
+                {
+                    double y = x / denominator;
+                    sum += y;
                 }
-                x++;
             }
 
             return Math.Round(sum, 3);
