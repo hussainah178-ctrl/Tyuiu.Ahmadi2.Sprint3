5e4d99d6e5ea9b48adaf047554ccd0683a4a9d2b
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-16 09:03:17 -0800

    Task1

--- Tyuiu.ahmadi2.Sprint3.Task1.V15.Lib/DataService.cs
+++ Tyuiu.ahmadi2.Sprint3.Task1.V15.Lib/DataService.cs
@@ -4,7 +4,7 @@ namespace Tyuiu.Ahmadi2.Sprint3.Task1.V15.Lib
 {
     public class DataService : ISprint3Task0V15
     {
-        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        public double GetMultiplySeries(int value, int startValue, int stopValue) 
         {
             double product = 1;
             int i = startValue;
