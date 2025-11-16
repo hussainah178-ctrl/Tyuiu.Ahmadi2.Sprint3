d1ea157d8ea7cdedfa712f23453595c62e2b6b34
Ahmadi1 <Ahmadi@DS10BK3>
2025-11-16 09:05:17 -0800

    TASK1

---
+++ Tyuiu.Ahmadi2.Sprint3.Task1.V15.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.Ahmadi2.Sprint3.Task1.V15.Lib;
+
+namespace Tyuiu.AhmadiM.Sprint3.Task1.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int value = 2;
+            int startValue = 1;
+            int stopValue = 5;
+            double wait = 208845.0;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue); 
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
